e7d2f529a6543787c4c4cae07921d8afc3c7e6a0
danicapis <daniele.pereira12@fatec.sp.gov.br>
2023-12-02 08:55:36 -0300

    EcoSmart

---
+++ Projeto-Integrador--master/Projeto-Top--master/Projeto/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBk-aKBP450iJO_RPXkzLZ6ldtROqgQ28I",
+    authDomain: "ecosmart-11322.firebaseapp.com",
+    projectId: "ecosmart-11322",
+    storageBucket: "ecosmart-11322.appspot.com",
+    messagingSenderId: "611248925992",
+    appId: "1:611248925992:web:317f9c2a8c13fb45a5f41d"
+  };
+firebase.initializeApp(firebaseConfig);
